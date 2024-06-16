99be6f1c42d481decb67504d92f3636031a0e857
AKKSHAAT <akkshaat@gmail.com>
2024-06-16 18:20:31 +0530

    fixed: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./*.html', './components/**/*.html'], // Add paths to all of your HTML files here
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
