89fee1ef38248ddcd03029f6e19cfe38dfe57439
waisrangrej <awaisrangrej9876@gmail.com>
2024-06-22 17:09:54 +0530

    ADDED:NEW MSG

--- utils.js
+++ utils.js
@@ -11,5 +11,9 @@ add('nav', 'components/nav.html');
 add('main', 'components/main.html')
 add('sec2', 'components/sec2.html')
 add('sec3', 'components/sec3.html')
+add('sec4', 'components/sec4.html')
+add('sec5', 'components/sec5.html')
+
+
 
 
