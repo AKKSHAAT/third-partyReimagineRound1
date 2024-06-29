f6b3cd24721d352ac91130507ff1732f9e29030f
waisrangrej <awaisrangrej9876@gmail.com>
2024-06-29 22:46:07 +0530

    newmsg

--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
 console.log('connected');
+(function () {
+    const locomotiveScroll = new LocomotiveScroll();
+})();
+
+
 function add(elementId, path) {
     fetch(path)
         .then(response => response.text())
@@ -11,7 +16,7 @@ function add(elementId, path) {
             }
         });
 }
-
+add('Landing', 'components/Landing.html')
 add('nav', 'components/nav.html');
 add('hero_section', 'components/hero_section.html');
 add('features', 'components/features.html');
@@ -20,6 +25,15 @@ add('aboutus', './components/aboutus.html');
 add('waffle_cakes', './components/waffle_cakes.html');    
 add('3d_waffle', 'components/3d_waffle.html')
 
-
+var tl=gsap.timeline()
+tl.to(".text-loader",{
+    opacity:1,
+    duration:0.7,
+    stagger:{
+        each:0.14,
+        repeat:1,
+        yoyo:true
+    }
+})
 
 
