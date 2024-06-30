3d545ff547712666c3dbce28084a77e02375060e
waisrangrej <awaisrangrej9876@gmail.com>
2024-06-30 15:45:45 +0530

    added:newmsg

--- animations.js
+++ animations.js
@@ -1,10 +1,12 @@
-var tl=gsap.timeline()
-tl.to(".text-loader",{
-    opacity:1,
-    duration:0.7,
-    stagger:{
-        each:0.14,
-        repeat:1,
-        yoyo:true
-    }
-})
+// var tl=gsap.timeline()
+// tl.to(".text-loader",{
+//     opacity:1,
+//     duration:0.7,
+//     stagger:{
+//         each:0.14,
+//         repeat:1,
+//         yoyo:true
+//     }
+// })
+
+
