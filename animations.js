f07876bfbfec7397b80964d8082d841bf10a8625
AKKSHAAT <akkshaat@gmail.com>
2024-06-30 22:16:02 +0530

    added: waffle in hand image

--- animations.js
+++ animations.js
@@ -8,6 +8,9 @@ function initAnimations() {
         gsap.from('#lol', {
             'background-color': 'red'
         });
+
+
+        
     }
 }
 
--- utils.js
+++ utils.js
@@ -15,15 +15,15 @@ function add(elementId, path) {
         });
 }
 
-// add('Landing', 'components/Landing.html');
-// add('nav', 'components/nav.html');
-// add('hero_section', 'components/hero_section.html');
-// add('features', 'components/features.html');
-// add('horizontal_scroll', './components/horizontal_scroll.html');
-// add('aboutus', './components/aboutus.html');
-// add('waffle_cakes', './components/waffle_cakes.html');
-// add('3d_waffle', 'components/3d_waffle.html');
-// add('regular_Waffles', 'components/regular_Waffles.html');
-// add('premix', 'components/premix.html');
-// add('product-List', './components/product-List.html');
+add('Landing', 'components/Landing.html');
+add('nav', 'components/nav.html');
+add('hero_section', 'components/hero_section.html');
+add('features', 'components/features.html');
+add('horizontal_scroll', './components/horizontal_scroll.html');
+add('aboutus', './components/aboutus.html');
+add('waffle_cakes', './components/waffle_cakes.html');
+add('3d_waffle', 'components/3d_waffle.html');
+add('regular_Waffles', 'components/regular_Waffles.html');
+add('premix', 'components/premix.html');
+add('product-List', './components/product-List.html');
 add('gsap', './components/gsap.html');
